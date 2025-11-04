87f880609aba99de929da78104f45798253fd019
Diana <korniyash@hotmail.com>
2025-11-04 12:33:09 +0100

    hw

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,28 @@
+import { Metadata } from "next";
 import css from "./page.module.css";
+import { IMG_URL, SITE_URL } from "@/lib/constants";
 
+
+export const metadata: Metadata = {
+  title: "404-Not found",
+  description:
+    "page not found",
+  openGraph: {
+    title: "404-Not found",
+    description:
+      "this page not found",
+    url: `${SITE_URL}/404`,
+
+    images: [
+      {
+        url: IMG_URL,
+        width: 1200,
+        height: 630,
+        alt: "page not found",
+      },
+    ],
+  },
+};
 const NotFound = () => {
   return (
     <>
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,11 +5,38 @@ import {
 } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api";
+import { Metadata } from "next";
+import { IMG_URL, SITE_URL } from "@/lib/constants";
+
+
+
 
 interface NotePageProps {
   params: Promise<{ slug: string[] }>;
 }
+export async function generateMetadata({ params }: NotePageProps): Promise<Metadata> {
+  const { slug } = await params
+  const category = slug[0] === "all" ? undefined : slug[0];
 
+  return {
+    title: category,
+    description: `Explore the best notes in the "${category}" category on NoteHub.`,
+    openGraph: {
+      title: category,
+      description: `Explore the best notes in the "${category}" category on NoteHub.`,
+      url: `${SITE_URL}/notes/filter/${slug[0]}`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: IMG_URL,
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub',
+        },
+      ]
+    }
+  }
+}
 export default async function NotePage({ params }: NotePageProps) {
   const { slug } = await params;
 
